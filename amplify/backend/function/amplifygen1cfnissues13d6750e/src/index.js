3b8bd711b65c9b0b809eb0e7cd4809c37fb15b54
Jon Wire <iambipedal@gmail.com>
2025-09-03 16:18:16 -0500

    updated function to v2

--- amplify/backend/function/amplifygen1cfnissues13d6750e/src/index.js
+++ amplify/backend/function/amplifygen1cfnissues13d6750e/src/index.js
@@ -12,6 +12,6 @@ exports.handler = async (event) => {
     //      "Access-Control-Allow-Origin": "*",
     //      "Access-Control-Allow-Headers": "*"
     //  },
-        body: JSON.stringify('Hello from Lambda!'),
+        body: JSON.stringify('Hello from Lambda! v2'),
     };
 };
